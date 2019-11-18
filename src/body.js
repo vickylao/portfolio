aa64eb2fb2658e1f675ce4487ec69b2d24f07ec0
vickylao <vickywklao@gmail.com>
2019-11-17 20:59:25 -0700

    added first body text :)

--- src/body.js
+++ src/body.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './body.css';
 
 function Body() {
     return (
     <div>
-        hi
+        <div className="Body-1">
+            <div className="Body-1-picture"/>
+            <div className="Body-1-text">
+                A technology professional with 5+ years of progressive experience in professional services consulting Fortune 500 companies across a wide range of industries. Passionate about delivering high-impact, value-added initiatives to achieve organizations’ overall strategic goals. 
+                <p></p>
+                Experienced in delivering projects simultaneously and proficient in utilizing Agile, Waterfall and Hybrid methodologies to strive for no-surprise implementations. Possesses a proven track record of project management, client relationship management, team coaching, and collaborating with remote teams while delivering quality implementations within budget and timeline.
+            </div>
+        </div>
+
     </div>
     )
 }
